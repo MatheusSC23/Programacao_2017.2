97d94278ac148028d7cc9bf5350c2c380c9e6eb0
Lukiam23 <matheus.1596@hot.com>
2017-09-13 13:04:35 -0300

    Create 1.c

---
+++ Lab02/1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int mdc(int x, int y){
+	int r = x%y;
+	if (r==0){
+		return y;
+	}
+	else{
+		return mdc(y,r);
+	}
+}
+int main(){
+	int escolha=0;
+	while(escolha!=2){
+		int x,y;
+		printf("Digite 1 para calcular o mdc e 2 para sair: ");
+		scanf("%d",&escolha);
+		if(escolha==1){
+			printf("Insira o primeiro valor: ");
+			scanf("%d",&x);
+			printf("Insira o segundo valor: ");
+			scanf("%d",&y);
+			printf("O mdc é %d\n",mdc(x,y));
+		}
+	}
+}
