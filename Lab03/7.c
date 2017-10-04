e442c17d0404607d764153cb3f3b84821fbd98a8
Matheus <mscorreia95@gmail.com>
2017-10-04 13:32:04 -0300

    Finalizado

--- Lab03/7.c
+++ Lab03/7.c
@@ -1,23 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-int Buscar(int num, int* v)
+int Buscar(int num, int* v, int len)
 {
-	return sizeof(v);
+	int i=0;
+	int achado = -1;
+	while(achado==-1 && i<len)
+	{
+		if(v[i]==num)
+		{
+			achado=i;
+		}
+		i++;
+	}
+	return achado;
 }
 
 
 int main()
 {
-	int n;
+	int n,num;
 	printf("Insira o valor de n: \n");
 	scanf("%d",&n);
 	int* v = (int*) malloc(n*sizeof(int));
 	for(int i=0;i<n;i++)
 	{
-		printf("Insira o elemendo %d: ",i+1);
+		printf("Insira o elemendo %d: ",i);
 		scanf("%d",&v[i]);
 	}
+	printf("Insira o valor a ser procurado: \n");
+	scanf("%d",&num);
+	int pos = Buscar(num,v,n);
+	if(pos!=-1)
+	{
+		printf("O número se encontra na posição %d\n", pos);
+	}
+	else
+	{
+		printf("O número não está na sequência.\n");
+	}
 	free(v);
-	printf("%d\n", Buscar(1,v));
 	return 0;
 }
