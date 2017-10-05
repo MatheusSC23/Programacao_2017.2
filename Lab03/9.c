65a34418add2a99269af6245812e9c1fb732ad52
Matheus <mscorreia95@gmail.com>
2017-10-05 16:22:46 -0300

    Finalizado

--- Lab03/9.c
+++ Lab03/9.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+int repeticoes(int num,int** matriz, int linhas, int colunas)
+{
+	int repeticoes;
+	repeticoes=0;
+	for(int i=0; i<linhas;i++)
+	{
+		for(int j=0; j<colunas; j++)
+		{
+			if(num==matriz[i][j])
+			{
+				repeticoes+=1;
+			}
+		}
+	}
+	return repeticoes;
+}
 
 int main()
 {
-	int linhas,colunas;
+	int linhas,colunas,num;
 	printf("Insira o número de colunas: \n");
 	scanf("%d",&colunas);
 	printf("Insira o número de linhas: \n");
@@ -24,8 +39,13 @@ int main()
 	{
 		for(int j=0; j<colunas;j++)
 		{
-			printf("%d",matriz[i][j]);
+			printf("%d ",matriz[i][j]);
 		}
+		printf("\n");
 	}
+	printf("Insira o número a se analisado: \n");
+	scanf("%d",&num);
+
+	printf("Vezes que %d apareceu: %d\n",num,repeticoes(num,matriz,linhas,colunas) );
 	return 0;
 }
