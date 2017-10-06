61153dd2ac6d7f5fff6a99fdebc50a6863f8970a
Matheus <mscorreia95@gmail.com>
2017-10-06 17:03:33 -0300

    Em andamento

---
+++ Lab03/12.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define linhas 3
+#define colunas 3
+typedef struct dados
+{
+	int win;
+	int winner;
+}dados; 
+dados verificar(int** a)
+{
+	int i=0;
+	int j=0;
+	int winner=-1;
+	int win=-1;
+	dados retorno;
+	while(i<3 && win==-1)
+	{
+		if(a[i][0]==a[i][1] && a[i][1]==a[i][2] && a[i][0]!=-1)
+		{
+			win=1;
+			winner=a[i][0];
+		}
+		i++;
+	}
+	while(j<3 && win==-1)
+	{
+		if(a[0][j]==a[1][j] && a[1][j]==a[2][j] && a[0][j]!=-1)
+		{
+			win=1;
+			winner=a[0][j];
+		}
+		i++;
+	}
+	if(a[0][0]==a[1][1] && a[1][1]==a[2][2] && a[0][j]!=-1)
+	{
+		win=1;
+	}
+	else if(a[0][2]==a[1][1] && a[1][1]==a[2][0] && a[0][2]=-1)
+	{
+		win=1;
+		winner=a[0][2];
+	}
+	retorno.win=win;
+	retorno.winner=winner;
+}
+
+int main()
+{
+	/*Cria a matriz A*/
+	int** matriz = (int**) malloc(linhas*sizeof(int*));
+	for(int i=0;i<linhas;i++)
+	{
+		matriz[i] = (int*) malloc(colunas*sizeof(int));
+		for(int j=0; j<colunas;j++)
+		{
+			matriz[i][j]=-1;
+		}
+	}
+	for(int i=0;i<linhas;i++)
+	{
+		printf("|%d %d %d|\n",matriz[i][0],matriz[i][1],matriz[i][2] );
+	}
+	printf("Vez do 0");
+	printf("Insira a coluna:\n");
+	return 0;
+}
