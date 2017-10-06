8605612b66e01a94ca201387210bedd2dd3e400e
Matheus <mscorreia95@gmail.com>
2017-10-06 15:47:43 -0300

    Finalizado

--- Lab03/11.c
+++ Lab03/11.c
@@ -1,34 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int produto(int** A;int** v, int linhas, int colunas)
-{
-	int repeticoes;
-	repeticoes=0;
-	for(int i=0; i<linhas;i++)
-	{
-		for(int j=0; j<colunas; j++)
-		{
-			if(num==matriz[i][j])
-			{
-				repeticoes+=1;
-			}
-		}
-	}
-	return repeticoes;
-}
-
-
 int main()
 {
 	int linhas,colunas,num;
-	printf("Insira o número de colunas: \n");
-	scanf("%d",&colunas);
 	printf("Insira o número de linhas: \n");
 	scanf("%d",&linhas);
-
-	int** R = (int**) malloc(linhas*sizeof(int*));
-	
+	printf("Insira o número de colunas: \n");
+	scanf("%d",&colunas);
+	/*Cria a matriz A*/
+	printf("Insira A:\n");	
 	int** matriz = (int**) malloc(linhas*sizeof(int*));
 	for(int i=0;i<linhas;i++)
 	{
@@ -39,24 +20,43 @@ int main()
 			scanf("%d",&matriz[i][j]);
 		}
 	}
-
-	int** v = (int**) malloc(linhas*sizeof(int*));
-	for(int i=0;i<linhas;i++)
+	printf("Insira v:\n");	
+	/*Cria a matriz v*/
+	int** v = (int**) malloc(colunas*sizeof(int*));
+	for(int i=0;i<colunas;i++)
 	{
+		v[i]= (int*) malloc(sizeof(int));
 		for(int j=0; j<1;j++)
 		{
 			printf("Insira o elemento da linha %d coluna %d de v:",i,j);
 			scanf("%d",&v[i][j]);
 		}
 	}
-	
+	/*Cria e calcula a matriz R*/
+	int** R = (int**) malloc(linhas*sizeof(int*));
+	for(int i=0; i<linhas;i++)
+	{
+		R[i]=(int*) malloc(sizeof(int));
+		for(int j=0; j<colunas; j++)
+		{
+			R[i][0]+=matriz[i][j]*v[j][0];
+
+		}
+	}
+	/*Imprime a matriz R*/
 	for(int i=0;i<linhas;i++)
 	{
-		for(int j=0; j<colunas;j++)
+		printf("|");
+		for(int j=0; j<1;j++)
 		{
-			printf("%d ",matriz[i][j]);
+			printf(" %d ",R[i][j]);
 		}
+		printf("|");
 		printf("\n");
 	}
+	free(matriz);
+	free(v);
+	free(R);
+	
 	return 0;
 }
