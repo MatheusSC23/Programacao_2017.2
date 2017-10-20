7c91099aab6fe4e4a0e41f884e04cbd47a9d7aec
Matheus <mscorreia95@gmail.com>
2017-10-20 11:03:47 -0300

    Em andamento

---
+++ Trabalho01/Aluno.h
@@ -0,0 +1,12 @@
+/* TAD: Aluno (matricula, nome, curso) */
+typedef struct aluno Aluno;
+/* Aloca e retorna um aluno com os dados passados por parâmetro */
+Aluno *novo_a(int matricula, char *nome, char *curso);
+/* Libera a memória de um aluno previamente criado */
+void libera_a(Aluno *aluno);
+/* Copia os valores de um aluno para as referências informadas */
+void acessa_a(Aluno *aluno, int *matricula, char *nome, char *curso);
+/* Atribui novos valores aos campos de um aluno */
+void atribui_a(Aluno *aluno, int matricula, char *nome, char *curso);
+/* Retorna o tamanho em bytes do TAD aluno */
+int tamanho_a();
