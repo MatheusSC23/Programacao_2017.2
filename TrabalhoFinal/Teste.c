38a302ea8ebde869604f45050182f949209a9adf
Matheus <mscorreia95@gmail.com>
2017-12-01 13:40:24 -0300

    Início do Teste, apenas a função testeNovoComDadosValidos() foi criada.

--- TrabalhoFinal/Teste.c
+++ TrabalhoFinal/Teste.c
@@ -3,3 +3,19 @@
 #include <stdlib.h>
 #include "Usuario.h"
 
+float nota;
+float testeNovoComDadosValidos(){
+	char nome[61], pais[31];
+	int dia,mes,ano,periodo,id;
+	float pontuacao = 0;
+
+	Viagem viagem = nova_v(23,12,2017,"Fortaleza","Brasil",10);
+	if(viagem != NULL){
+		printf("[Passou 1.50] - Teste da função nova_v() com dados válidos!\n");
+		pontuacao = 1.5;
+	}
+	else{
+		printf("[Falhou 0.00] - Teste da função nova_v() com dados válidos!\n");
+	}
+	return pontuacao;
+}
